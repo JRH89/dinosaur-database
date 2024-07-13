298d82f1483bc7385a27068667fdbc1475153a6d
JRH89 <thegemlins@gmail.com>
2024-07-12 19:00:29 -0700

    ok

--- src/components/Query.jsx
+++ src/components/Query.jsx
@@ -17,7 +17,7 @@ export default function Query() {
     const fetchDinosaurs = async () => {
         const encodedQuery = encodeURIComponent(query);
         try {
-            const res = await fetch(`/api/dinosaurs/${searchType}/${encodedQuery}`);
+            const res = await fetch(`https://dinosaur-database.vercel.app/api/dinosaurs/${searchType}/${encodedQuery}`);
             if (!res.ok) {
                 throw new Error('Network response was not ok');
             }
